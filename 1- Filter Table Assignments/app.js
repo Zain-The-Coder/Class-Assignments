df12a3490402d7882db18abc252879380de755e4
Zain-The-Coder <zain015976@gmail.com>
2025-11-01 01:42:49 +0500

    Assignment 1

---
+++ 1- Filter Table Assignments/app.js
@@ -0,0 +1,9 @@
+let itemName = document.getElementById("item-name");
+let itemCategorey = document.getElementById("item-categorey");
+let itemType = document.getElementById("item-type")
+let table = document.getElementById("table");
+
+function button1 () {
+    itemName.style.color = "blue" ;
+    console.log(table.childNodes[1].childNodes)
+}
