fa5873f756ec4c3f0e4f5021e8fff9711cb5a73b
Zain-The-Coder <zain015976@gmail.com>
2025-11-01 21:59:17 +0500

    Assignment 1

--- 1- Filter Table Assignments/app.js
+++ 1- Filter Table Assignments/app.js
@@ -1,9 +1,58 @@
 let itemName = document.getElementById("item-name");
 let itemCategorey = document.getElementById("item-categorey");
-let itemType = document.getElementById("item-type")
+let itemType = document.getElementById("item-type");
+let userInput = document.getElementById("userInput");
 let table = document.getElementById("table");
+let firstButton = document.getElementById("firstButton");
+let secondButton = document.getElementById("secondButton");
+let thirdButton = document.getElementById("thirdButton");
+
+let ifFilter = "" ;
 
 function button1 () {
+    ifFilter = "name"
+    firstButton.style.backgroundColor = "bisque" ;
     itemName.style.color = "blue" ;
-    console.log(table.childNodes[1].childNodes)
+    userInput.addEventListener("keyup" , () => {
+
+        if(ifFilter !== "name") {
+            return ;
+        }
+        let itemsName = table.childNodes[1].childNodes;
+
+        for(let i = 0 ; i < itemsName.length ; i++) {
+            if(itemsName[i].nodeType === 1) {
+                let tableRow = itemsName[i]
+                let elementName = (tableRow.childNodes[0].textContent);
+                if(elementName.toLowerCase().indexOf(userInput.value) !== -1) {
+                    tableRow.style.display = "" ;
+                } else {
+                    tableRow.style.display = "none";
+                }
+            }
+        }        
+    })
+}
+
+function button2 () {
+    ifFilter = "categorey";
+    secondButton.style.backgroundColor = "bisque" ;
+    itemCategorey.style.color = "blue" ;
+    userInput.addEventListener("keyup" , () => {
+        if(ifFilter !== "categorey") {
+            return ;
+        }
+        let itemsName = (table.childNodes[1].childNodes);
+        for(let i = 0 ; i < itemsName.length; i++) {
+            if(itemsName[i].nodeType === 1){
+                let tableRow = itemsName[i];
+                let elementCategorey = (tableRow.childNodes[1].textContent);
+                if(elementCategorey.toLowerCase().indexOf(userInput.value) !== -1) {
+                    tableRow.style.display = ""
+                } else {
+                    tableRow.style.display = "none" ;
+                }
+            }
+        }
+    })
 }
