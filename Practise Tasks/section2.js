6834d754ddbb1693c4fd00ad490c310b26ce5eac
Zain-The-Coder <zain015976@gmail.com>
2026-01-30 01:42:00 +0500

    Class Assignment

---
+++ Practise Tasks/section2.js
@@ -0,0 +1,34 @@
+//                    Section 2: Template Literals (Practical Usage)
+
+
+
+//Task 2.1 – Dynamic Message Generator
+// const inputTaker = () => {
+//     let userName = prompt(`Welcome User , Enter Your Name`);
+//     let courseName = prompt(`Hey ${userName} Enter The Course Name In Which You Enrolled...`)
+//     let number = +prompt(`How Much Number You Gained Out Of 100`);
+//     let percentage = `Your Percentage Is ${(number / 100) * 100}% `
+
+//     let finalResult = `Welcome ${userName}
+// Your Course Is : ${courseName}
+// ${percentage}`;
+
+//     return finalResult;
+// }
+// console.log(inputTaker())
+
+
+//Task 2.2 – Conditional Templates
+// let userName = prompt(`Enter Your Name`);
+// let userMarks = +prompt(`Enter Your Total Marks Out Of 250`);
+// let percentage = (userMarks / 250) * 100
+
+// if(percentage > 50) {
+//     console.log(`Hey ${userName} , You Scored ${percentage}%
+//     Status => Passed`)
+// } else {
+//     console.log(`Hey ${userName} , You Scored ${percentage}% 
+//     Status => Failed`);
+// }
+
+
---
+++ Practise Tasks/section3.js
@@ -0,0 +1,19 @@
+//                 Section 3: Arrow Functions (Behavior Matters)
+
+
+
+// Task 3.1 – Arrow vs Regular Function
+// const object = {
+//     name : `zain ur rehman` ,
+
+//     normalFunction : function () {
+//         console.log(this.name);
+//     },
+//     arrowFunction : () => {
+//         console.log(this.name)
+//     }
+// }
+// object.normalFunction();
+// object.arrowFunction()
+
+
