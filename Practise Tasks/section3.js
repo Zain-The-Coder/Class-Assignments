3687ca9bf168b1c91d4ede294be2c06372ed4d5d
Zain-The-Coder <zain015976@gmail.com>
2026-01-31 18:23:17 +0500

    Practise Tasks Of JavaScript

--- Practise Tasks/section3.js
+++ Practise Tasks/section3.js
@@ -14,6 +14,33 @@
 //     }
 // }
 // object.normalFunction();
-// object.arrowFunction()
+// object.arrowFunction();
 
+
+//Task 3.2 – Implicit vs Explicit Return
+// const web = ["HTML" , "CSS" , "JS"];
+
+// //Yaha Return Keyword ke need ni because without curly braces 1 line may kaam hogaya !`
+// const implicitFunction = web.map(item => `The Currunt Item Is ${item}`);
+
+// //Yaha Need Agaye because Braces Use krliye.
+// const explicitFunction = web.map(item => {
+//     return `The Currunt Item Is ${item}`
+// })
+
+// console.log(implicitFunction);
+// console.log(explicitFunction);
+
+
+//Task 3.3 – Arrow Functions in Callbacks
+// const web = ["HTML" , "CSS" , "JS"];
+
+// const arrowFunction = () => {
+//     console.log(`Arrow Function Run`);
+// } 
+// function normalFunction () {
+//     console.log(`Normal Function Run`);
+// }
+// setTimeout(arrowFunction , 1000);
+// setTimeout(normalFunction , 1000);
 
