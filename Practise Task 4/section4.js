217ff325db352266c63f6649ae1d0db640ac4e1f
Zain-The-Coder <zain015976@gmail.com>
2026-02-07 21:10:58 +0500

    ES6 Assignment

--- Practise Task 4/section4.js
+++ Practise Task 4/section4.js
@@ -2,39 +2,39 @@
 
 
 // //Task 4.1 – Sync vs Async Execution Order
-// console.log("program start");
+console.log("program start");
 
-// function first () {
-//     console.log("inside normal function");
-// }
-// first();
-// setTimeout(() => {
-//     console.log("i am run with (0ms) late");
-// }, 0);
+function first () {
+    console.log("inside normal function");
+}
+first();
+setTimeout(() => {
+    console.log("i am run with (0ms) late");
+}, 0);
 
-// setTimeout(() => {
-//     console.log("i am run with (300 ms) late")
-// }, 300);
+setTimeout(() => {
+    console.log("i am run with (300 ms) late")
+}, 300);
 
-// console.log("program end");
+console.log("program end");
 
 
 // // Task 4.2 – Callback Hell Simulation
-// setTimeout(() => {
-//     console.log("i am run at 100ms");
-//     setTimeout(() => {
-//         console.log("i am run at 200ms");
-//         setTimeout(() => {
-//             console.log("i am run at 300ms");
-//             setTimeout(() => {
-//                 console.log("i am run at 400ms");
-//                 setTimeout(() => {
-//                     console.log("i am run at 500ms");
-//                 }, 500);
-//             }, 400);
-//         }, 300);
-//     }, 200);
-// }, 100);
+setTimeout(() => {
+    console.log("i am run at 100ms");
+    setTimeout(() => {
+        console.log("i am run at 200ms");
+        setTimeout(() => {
+            console.log("i am run at 300ms");
+            setTimeout(() => {
+                console.log("i am run at 400ms");
+                setTimeout(() => {
+                    console.log("i am run at 500ms");
+                }, 500);
+            }, 400);
+        }, 300);
+    }, 200);
+}, 100);
 
 
 // Task 4.3 – Promises vs Callbacks (Hard)
